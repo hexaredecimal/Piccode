b0f9fce890581fbdf77990ade1296d9cc510207a
hexaredecimal <mfanakagama@gmail.com>
2025-05-26 20:44:01 +0200

    piccodescript: Bump the version up to 0.2

--- src/main/java/org/piccode/piccodescript/Piccode.java
+++ src/main/java/org/piccode/piccodescript/Piccode.java
@@ -24,7 +24,7 @@ import org.piccode.rt.PiccodeValue;
  */
 public class Piccode {
 
-  private static double VERSION = 0.1;
+  private static double VERSION = 0.2;
 
 	public static void main(String[] args) {
 		run(args);
