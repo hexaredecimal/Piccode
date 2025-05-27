4366c3bc36ea71b75afb91b01d3fa57cb44be41c
hexaredecimal <mfanakagama@gmail.com>
2025-05-27 22:34:26 +0200

    verify: Run all examples in parallel

--- verify.sh
+++ verify.sh
@@ -4,7 +4,7 @@ echo "[LOG]: Starting"
 ls ./examples/*.pics | while read line 
 do
 	printf "[LOG]: Running file %s\n" $line
-	java -jar target/PiccodeScript-0.1-jar-with-dependencies.jar run $line > /dev/null
+	java -jar target/PiccodeScript-0.1-jar-with-dependencies.jar run $line > /dev/null &
 done
 
 echo "[LOG]: Done"
