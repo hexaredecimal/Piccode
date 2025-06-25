16d2dec293232daf389460a4b8a99406bd855c87
hexaredecimal <mfanakagama@gmail.com>
2025-06-25 19:40:54 +0200

    editors: Add syntax highlighting for light editor

---
+++ editors/Lite/language_piccodescript.lua
@@ -0,0 +1,32 @@
+local syntax = require "core.syntax"
+
+syntax.add {
+  files = { "%.pics$"},
+  comment = "//",
+  patterns = {
+    { pattern = "//.-\n",               type = "comment"  },
+    { pattern = { "/%*", "%*/" },       type = "comment"  },
+    { pattern = { '"', '"', '\\' },     type = "string"   },
+    { pattern = { "'", "'", '\\' },     type = "string"   },
+    { pattern = "-?0x%x+",              type = "number"   },
+    { pattern = "-?%d+[%d%.eE]*f?",     type = "number"   },
+    { pattern = "-?%.?%d+f?",           type = "number"   },
+    { pattern = "[%+%-=/%*%^%%<>!~|&]", type = "operator" },
+    { pattern = "[%a_][%w_]*%f[(]",     type = "function" },
+    { pattern = "[%a_][%w_]*",          type = "symbol"   },
+  },
+  symbols = {
+    ["import"]         = "keyword",
+    ["module"]         = "keyword",
+    ["function"]       = "keyword",
+    ["let"]             = "keyword",
+    ["if"]             = "keyword",
+    ["else"]           = "keyword",
+    ["do"]             = "keyword",
+    ["when"]          = "keyword",
+    ["is"]             = "keyword",
+    ["true"]           = "literal",
+    ["false"]          = "literal",
+  },
+}
+
