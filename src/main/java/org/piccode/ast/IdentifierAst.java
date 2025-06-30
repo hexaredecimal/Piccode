5793c7214a13124df117b1990055413d4ccf0774
hexaredecimal <mfanakagama@gmail.com>
2025-06-30 11:47:24 +0200

    ast: Improve the display of the symbol dump

--- src/main/java/org/piccode/ast/IdentifierAst.java
+++ src/main/java/org/piccode/ast/IdentifierAst.java
@@ -28,12 +28,11 @@ public class IdentifierAst extends Ast {
 	@Override
 	public PiccodeValue execute(Integer frame) {
 		var ctx = frame == null
-			? Context.top
-			: Context.getContextAt(frame);
+						? Context.top
+						: Context.getContextAt(frame);
 
-		
 		var value = ctx.getValue(text);
-		
+
 		if (value == null) {
 			var err = new PiccodeException(file, line, column, "Unknown variable `" + Chalk.on(text).red() + "` ");
 			err.frame = frame;
@@ -42,28 +41,54 @@ public class IdentifierAst extends Ast {
 				var note = new PiccodeSimpleNote("Did you mean `" + Chalk.on(nm).green() + "` instead of `" + Chalk.on(text).red() + "` ?");
 				err.addNote(note);
 			}
-			
+
 			var stack = ctx.getTopFrame().toMap();
 			var sb = new StringBuilder();
+			var nms = new StringBuilder();
 			var entrySet = stack.entrySet();
 			var size = entrySet.size();
-			var index  = 0;
-			for (var kv: entrySet) {
+			var index = 0;
+			var count = 1;
+
+			for (var kv : entrySet) {
 				var key = kv.getKey();
-				sb.append(key);
+				nms.append(key);
+				var val = kv.getValue().hashCode();
+				var fmt = String.format("0x%08X", val);
+				sb.append(fmt);
+
 				if (index < size - 1) {
-					sb.append(" ,");
+					sb.append(" ");
+					nms.append(" ");
 				}
-				var str = sb.toString().length();
-				if (index % 5 == 0) {
+				count++;
+
+				if (index + 1 == size) {
+					var split = sb.toString().split("\n");
+					if (split.length > 0 && count < 3) {
+						var part = split[0].split("|")[1];
+						var lastSplt = split[split.length - 1].split("|");
+						var last = lastSplt.length == 1 ? lastSplt[0] : lastSplt[lastSplt.length - 1];
+						var padd = " ".repeat(part.length() - last.length());
+						sb.append(padd)
+							.append(" | ")
+							.append(nms.toString().trim());
+						count=1;
+					}
+				} else if ((index + 1) % 3 == 0) {
+					count =1;
+					sb
+									.append(" | ")
+									.append(nms.toString().trim());
 					sb.append("\n");
+					nms = new StringBuilder();
 				}
 				index++;
 			}
-			
-			var note = new PiccodeSimpleNote("Track size: " + ctx.getFramesCount());
+
+			var note = new PiccodeSimpleNote("Stack size: " + ctx.getFramesCount());
 			err.addNote(note);
-			
+
 			note = new PiccodeSimpleNote("Symbol table dump: \n" + sb.toString());
 			err.addNote(note);
 			throw err;
@@ -71,7 +96,7 @@ public class IdentifierAst extends Ast {
 
 		if (value instanceof NativeFunction nat) {
 			nat.frame = frame;
-		} 
+		}
 		return value;
 	}
 
