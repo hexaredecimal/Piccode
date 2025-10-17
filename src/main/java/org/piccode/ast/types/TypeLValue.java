9a89dbc0dd78b6a8d7b0a6af5fa63a0ed8da2ec7
hexaredecimal <mfanakagama@gmail.com>
2025-10-17 14:18:20 +0200

    TypeLValue: Handle a generic typelvalue

--- src/main/java/org/piccode/ast/types/TypeLValue.java
+++ src/main/java/org/piccode/ast/types/TypeLValue.java
@@ -1,11 +1,14 @@
 package org.piccode.ast.types;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.piccode.ast.Ast;
+import org.piccode.ast.IdentifierAst;
 import org.piccode.errors.PiccodeException;
 import org.piccode.typechecker.Context;
 import org.piccode.typechecker.TypeCheckable;
 import org.piccode.typechecker.type.Type;
+import org.piccode.typechecker.type.UserType;
 
 /**
  *
@@ -24,15 +27,45 @@ public class TypeLValue extends Ast implements TypeCheckable {
 	@Override
 	public Type getType(Context ctx) {
 		var typeTable = ctx.getTypeTable();
-		
+		var savedType = typeTable.getValueForSymbol(name);
+
+		if (savedType == null) {
+			throw new PiccodeException(file, line, column, "Usage of undeclared type `" + name + "`");
+		}
+
 		if (genericTypes.isEmpty()) {
-			var savedType = typeTable.getValueForSymbol(name);
-			if (savedType == null) {
-				throw new PiccodeException(file, line, column, "Usage of undeclared type `" + name + "`");
-			}
 			return savedType;
 		}
 
-		throw new PiccodeException(file, line, column, "TODO: Implement generic types");
+		if (savedType instanceof UserType userType) {
+			var expected = userType.genericParams.size();
+			var found = genericTypes.size();
+			if (found != expected) {
+				var error = new PiccodeException(file, line, column, "Invalid number of generic parameters passed to type `" + name + "`. Expected " + expected + " but found " + found + " ");
+				var declNode = userType.getDeclaringNode();
+				error.addNote(new PiccodeException(declNode.file, declNode.line, declNode.column, "The generic type is declared here"));
+				throw error;
+			}
+
+			genericTypes.forEach(genericType -> verify(genericType, ctx));
+		}
+
+		return savedType;
 	}
+
+
+	private void verify(Ast generic, Context ctx) {
+		var typeTable = ctx.getTypeTable();
+		if (generic instanceof IdentifierAst id) {
+			var saved = typeTable.getValueForSymbol(id.text);
+			if (saved == null) {
+				throw new PiccodeException(id.file, id.line, id.column, "Type `" + id.text + "` does not exists");
+			}
+			return;
+		}
+
+		var typedNode = (TypeCheckable) generic;
+		typedNode.getType(ctx);
+	}
+
 }
